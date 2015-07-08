e86d071534829f2c291840284f2c91e4af98db43
david <davecusatis1@gmail.com>
2015-07-07 23:48:58 -0400

    added database schema, and backend connector django side

--- monospace/settings.py
+++ monospace/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'monospace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'monospace',
+        'USER': os.environ.get('DB_USR'),
+        'PASSWORD': os.environ.get('DB_PASSWD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
